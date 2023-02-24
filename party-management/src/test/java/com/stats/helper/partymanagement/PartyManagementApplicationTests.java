726e35b015cf731b65df9785bb189b752dfac825
slavko <firepoint.pro@gmail.com>
2023-02-24 11:58:11 +0300

    Initial commit

---
+++ party-management/src/test/java/com/stats/helper/partymanagement/PartyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stats.helper.partymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ report-management/src/main/java/com/stats/helper/reportmanagement/ReportManagementApplication.java
@@ -0,0 +1,13 @@
+package com.stats.helper.reportmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportManagementApplication.class, args);
+	}
+
+}
